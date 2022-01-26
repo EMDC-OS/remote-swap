b1c9b3f665f2101d10c21f4fd8fb8e6b5be48167
Jinyong Park <jinyong.park@csi.skku.edu>
2022-01-26 12:45:27 +0900

    initial2

---
+++ msm-google/include/linux/app_aware.h
@@ -0,0 +1,34 @@
+#ifndef _LINUX_APP_AWARE_H
+#define _LINUX_APP_AWARE_H
+
+
+extern int swapcache_flush;
+extern int swapcache_flush_handler(struct ctl_table *table, int write,
+                 void __user *buffer, size_t *length, loff_t *ppos);
+
+
+extern int workingset_status;
+extern int workingset_status_handler(struct ctl_table *table, int write,
+                 void __user *buffer, size_t *length, loff_t *ppos);
+
+extern int anon_uid_scan;
+extern int anon_uid_scan_handler(struct ctl_table *table, int write,
+                 void __user *buffer, size_t *length, loff_t *ppos);
+
+extern int find_lru_index_mode;
+extern int find_lru_index;
+extern int find_lru_index_handler(struct ctl_table *table, int write,
+                 void __user *buffer, size_t *length, loff_t *ppos);
+
+extern int lru_scan_private;
+extern int lru_scan_private_handler(struct ctl_table *table, int write,
+                 void __user *buffer, size_t *length, loff_t *ppos);
+
+extern int lru_scan;
+extern int lru_scan_handler(struct ctl_table *table, int write,
+                 void __user *buffer, size_t *length, loff_t *ppos);
+
+extern int foreground_uid;
+extern int foreground_pid;
+extern int swapin_vma_tracking;
+#endif /* _LINUX_APP_AWARE_H */
